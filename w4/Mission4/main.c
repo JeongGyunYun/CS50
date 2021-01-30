04fc8e4adff9c2e3f9deae5ff0e3a4600beb3684
JeongGyunYun <jeongkun3@gmail.com>
2021-01-30 13:54:04 +0900

    가장 큰 낙하거리 찾기

---
+++ w4/Mission4/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef struct{
+    int wPos;
+    int hPos;
+}Point;
+
+void set_room(int *room[8], int pos, int cnt);
+
+
+int main() {
+    int width = 9, height = 8;
+    int inputSet[] = {7, 4, 2, 0, 0, 6, 0, 7, 0};
+    Point boxSet[9];
+    int max = 0;
+
+    for(int i = 0; i<width; i++){
+
+    }
+
+
+
+    return 0;
+}
+
+void set_room(int **room, int pos, int cnt) {
+
+}
+
+
--- w4/m1/main.c
+++ w4/m1/main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <math.h>
+#define SIZE 5
 
-int main() {
-    printf("Hello, World!\n");
+
+
+int main(void) {
+    int a = 12345;
+    //TODO 숫자를 배열 형태로 변환
+
     return 0;
 }
+
+void swap(int *arr, int id1, int id2) {
+    if (id1 == id2) return;
+    arr[id1] ^= arr[id2];
+    arr[id2] ^= arr[id1];
+    arr[id1] ^= arr[id2];
+}
+
+
+/**
+ *
+ * @param arr   정렬되지 않은 배열
+ * @param left  배얼의 첫 번째 index
+ * @param right 배열의 두 번째 index
+ */
+void quick_sort(int *arr, int left, int right) {
+    int toLeft, toRight, pivot;
+
+    if(left < right) {
+        pivot = arr[left];
+        toRight = left;
+        toLeft = right + 1;
+        do {
+            do toRight++; while (pivot > arr[toRight]);
+            do toLeft--; while (pivot < arr[toLeft]);
+            if (toRight < toLeft) swap(arr, toRight, toLeft);
+        } while (toRight < toLeft);
+        swap(arr, left, toLeft);
+        quick_sort(arr, left, toLeft - 1);
+        quick_sort(arr, toLeft + 1, right);
+    }
+}
+
+
