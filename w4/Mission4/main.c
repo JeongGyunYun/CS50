16d89a703a7fd55d02b0045e6502bf276a07aafe
JeongGyunYun <jeongkun3@gmail.com>
2021-02-01 14:41:15 +0900

    stable sort 이용한 낙하거리 구하기

--- w4/Mission4/main.c
+++ w4/Mission4/main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-void swap(int *arr, int id1, int id2);
-
-void sort(int *wInfo, int *hInfo, int left, int right);
-
+#define SWAP(x, y, z) (z)=(x), (x)=(y), (y)=(z)
 void prtArr(int *wInfo, int *hInfo, int w);
+void buble_sort(int *lenArr,int *indexArr ,int cnt);
+
 
 int main() {
     char filePath[100];
@@ -21,52 +19,41 @@ int main() {
     fscanf(f, "%d", &h);
 
     int* hInfo = (int *)malloc(sizeof (*hInfo) * w);
-    int* wInfo = (int *)malloc(sizeof (*hInfo) * w);
+    int* wInfo = (int *)malloc(sizeof (*wInfo) * w);
+    int* posTable = (int *)calloc(w, sizeof(*posTable));
 
     for(int i = 0; i < w; i++){
         fscanf(f, "%d", &hInfo[i]);
         wInfo[i] = i;
     }
 
-    sort(wInfo, hInfo, 0, w - 1);
+    buble_sort(hInfo, wInfo, w);
     prtArr(wInfo, hInfo, w);
+    puts("");
 
-    //TODO hInfo배열에서 가장 큰 값만 찾아 index 저장
-    //TODO index에 해당하는 wInfo배열 값을 새로운 배열에 저장, arr[0] = width
-
+    int maxCntIndex = 0;
+    for(int i = w-1; hInfo[i]>=hInfo[w-1]; i--, maxCntIndex++); //Max값을 갖는 원소 중 가장 작은 index값을 구함;
+    printf("%d\n", w - maxCntIndex);
+    int minIndex = w - maxCntIndex;
+    printf("%d\n", w-maxCntIndex-wInfo[minIndex]);
 
     free(hInfo);
     free(wInfo);
+    free(posTable);
     fclose(f);
     return 0;
 }
 
-void swap(int *arr, int id1, int id2) {
-    if (id1 == id2) return;
-    arr[id1] ^= arr[id2];
-    arr[id2] ^= arr[id1];
-    arr[id1] ^= arr[id2];
-}
 
-void sort(int *wInfo, int *hInfo, int left, int right) {
-    int toLeft, toRight, pivot;
-
-    if(left < right) {
-        pivot = hInfo[left];
-        toRight = left;
-        toLeft = right + 1;
-        do {
-            do toRight++; while (pivot > hInfo[toRight]);
-            do toLeft--; while (pivot < hInfo[toLeft]);
-            if (toRight < toLeft) {
-                swap(hInfo, toRight, toLeft);
-                swap(wInfo, toRight, toLeft);
+void buble_sort(int *lenArr,int *indexArr ,int cnt){
+    int temp = 0;
+    for(int i = cnt; i >=0; i--){
+        for(int j = 1; j<i; j++){
+            if(lenArr[j]<lenArr[j-1]){
+                SWAP(lenArr[j], lenArr[j-1], temp);
+                SWAP(indexArr[j], indexArr[j-1], temp);
             }
-        } while (toRight < toLeft);
-        swap(hInfo, left, toLeft);
-        swap(wInfo, left, toLeft);
-        sort(wInfo, hInfo, left, toLeft - 1);
-        sort(wInfo, hInfo, toLeft + 1, right);
+        }
     }
 }
 
